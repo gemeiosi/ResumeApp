80e34aae08782fd08a2229c806ebaf999aed2ebf
Iosif Gemenitzoglou <iosif.gemenitzoglou@gmail.com>
2024-10-16 17:09:59 +0300

    candidate and degree entities created

---
+++ Data/Models/Candidate.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace ResumeApp.Data.Models
+{
+    public class Candidate
+    {
+        public int Id { get; set; }
+        public string LastName { get; set; }
+        public string FirstName { get; set; }
+        public string? Email { get; set; }
+        public string? Mobile { get; set; }
+        public string? Degree { get; set; }
+        public string?  CV { get; set; }
+        public DateTime? CreationTime { get; set; } = DateTime.Now;
+    }
+}
---
+++ Data/Models/Degree.cs
@@ -0,0 +1,9 @@
+﻿namespace ResumeApp.Data.Models
+{
+    public class Degree
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime? CreationTime { get; set; } = DateTime.Now;
+    }
+}
