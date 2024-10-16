5bd276945fd2f70ff290dec38354e59213ac603f
Iosif Gemenitzoglou <iosif.gemenitzoglou@gmail.com>
2024-10-16 17:23:26 +0300

    candidate and degree services created

---
+++ Data/Services/CandidateService.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ResumeApp.Data.Interfaces;
+using ResumeApp.Data.Models;
+using System.Linq;
+
+namespace ResumeApp.Data.Services
+{
+    public class CandidateService : ICandidateService
+    {
+        private readonly AppDBContext _context;
+
+        public CandidateService(AppDBContext context)
+        {
+            _context = context;
+        }
+        public void AddCandidate(Candidate newCandidate)
+        {
+            Console.WriteLine(newCandidate.Id);
+            _context.candidate.Add(newCandidate);
+            _context.Entry(newCandidate).State = EntityState.Added;
+            _context.SaveChangesAsync();
+        }
+
+        public void DeleteCandidate(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public async Task<List<Candidate>> GetAllCandidates()
+        {
+            var results = new List<Candidate>();
+            return await _context.candidate.ToListAsync();
+        }
+
+        public Candidate GetCandidateById(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void UpdateCandidate(int id, Candidate newCandidate)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ Data/Services/DegreeService.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ResumeApp.Data.Interfaces;
+using ResumeApp.Data.Models;
+using System.Linq;
+
+namespace ResumeApp.Data.Services
+{
+    public class DegreeService : IDegreeService
+    {
+        private readonly AppDBContext _context;
+
+        public DegreeService(AppDBContext context)
+        {
+            _context = context;
+        }
+        public void AddDegree(Degree newDegree)
+        {
+            _context.degree.Add(newDegree);
+            _context.SaveChangesAsync();
+        }
+
+        public void DeleteDegree(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public async Task<List<Degree>> GetAllDegrees()
+        {
+            var results = new List<Degree>();
+            return await _context.degree.ToListAsync();
+        }
+
+        public Degree GetDegreeById(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void UpdateDegree(int id, Degree newDegree)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
