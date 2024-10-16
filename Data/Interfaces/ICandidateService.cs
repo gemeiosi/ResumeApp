bc072763b933cbe5e8d5dfd375cc04d6d9d40774
Iosif Gemenitzoglou <iosif.gemenitzoglou@gmail.com>
2024-10-16 17:23:01 +0300

    candidate and degree interfaces implemented

---
+++ Data/Interfaces/ICandidateService.cs
@@ -0,0 +1,13 @@
+﻿using ResumeApp.Data.Models;
+
+namespace ResumeApp.Data.Interfaces
+{
+    public interface ICandidateService
+    {
+        Task<List<Candidate>> GetAllCandidates();
+        Candidate GetCandidateById(int id);
+        void UpdateCandidate(int id, Candidate candidate);
+        void DeleteCandidate(int id);
+        void AddCandidate(Candidate newCandidate);
+    }
+}
---
+++ Data/Interfaces/IDegreeService.cs
@@ -0,0 +1,13 @@
+﻿using ResumeApp.Data.Models;
+
+namespace ResumeApp.Data.Interfaces
+{
+    public interface IDegreeService
+    {
+        Task<List<Degree>> GetAllDegrees();
+        Degree GetDegreeById(int id);
+        void UpdateDegree(int id, Degree degree);
+        void DeleteDegree(int id);
+        void AddDegree(Degree newdegree);
+    }
+}
