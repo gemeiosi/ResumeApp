576852c6ecb50d5f09c79e3012beab80766b7148
Iosif Gemenitzoglou <iosif.gemenitzoglou@gmail.com>
2024-10-16 17:22:29 +0300

    candidatedto and degreedto created

---
+++ Data/Dtos/CandidateDto.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ResumeApp.Data.Dtos
+{
+    public class CandidateDto
+    {
+        public int Id { get; set; }
+        [Required]
+        public string FirstName { get; set; }
+        [Required]
+        public string LastName { get; set; }
+        public string? Email { get; set; }
+        public string? Mobile { get; set; }
+        public string? Degree { get; set; }
+        public string?  CV { get; set; }
+        public DateTime? CreationTime { get; set; }
+    }
+}
---
+++ Data/Dtos/DegreeDto.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ResumeApp.Data.Dtos
+{
+    public class DegreeDto
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public DateTime? CreationTime { get; set; } = DateTime.Now;
+    }
+}
