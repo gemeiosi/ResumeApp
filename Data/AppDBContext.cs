79eb79e3688abd7868a55366c3cfaf2ab4c2348f
Iosif Gemenitzoglou <iosif.gemenitzoglou@gmail.com>
2024-10-16 17:09:22 +0300

    ef in memory installed and appdbcontext created

---
+++ Data/AppDBContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ResumeApp.Data.Models;
+
+namespace ResumeApp.Data
+{
+    public class AppDBContext(DbContextOptions<AppDBContext> options) : DbContext(options)
+    {
+    }
+}
