68cb128930939c926e8cee973d3446f3f33c50d9
Iosif Gemenitzoglou <iosif.gemenitzoglou@gmail.com>
2024-10-16 17:23:49 +0300

    automapper installed and  created mappers

---
+++ Data/Profiles/DataProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using ResumeApp.Data.Dtos;
+using ResumeApp.Data.Models;
+
+namespace ResumeApp.Data.Profiles
+{
+    public class DataProfile : Profile
+    {
+        public DataProfile()
+        {
+            CreateMap<Candidate, CandidateDto>().ReverseMap();
+            CreateMap<Degree,DegreeDto>().ReverseMap();
+        }
+        
+    }
+}
