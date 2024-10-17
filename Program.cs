f7af3836ebcc3ed41c1091dddb0827746d216d3e
Iosif Gemenitzoglou <iosif.gemenitzoglou@gmail.com>
2024-10-17 08:41:45 +0300

    fixes

--- Program.cs
+++ Program.cs
@@ -1,16 +1,42 @@
+using Microsoft.EntityFrameworkCore;
+using ResumeApp.Data.Interfaces;
+using ResumeApp.Data.Services;
+using ResumeApp.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllersWithViews();
+builder.Services.AddTransient<ICandidateService, CandidateService>();
+builder.Services.AddTransient<IDegreeService, DegreeService>();
+builder.Services.AddDbContext<AppDBContext>(options =>
+{
+    options.UseInMemoryDatabase(builder.Configuration.GetConnectionString("ResumeMngDB")).UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);
+});
+builder.Services.AddAutoMapper(typeof(Program));
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddCors(options =>
+{
+
+    options.AddDefaultPolicy(
+        policy =>
+        {
+            policy.AllowAnyOrigin()
+                .AllowAnyHeader()
+                .AllowAnyMethod();
+        });
+});
 
 var app = builder.Build();
+app.UseCors();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
+    
 }
 
 app.UseHttpsRedirection();
